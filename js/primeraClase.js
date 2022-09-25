371aede42c9d4195e81cca90dac07a487c641f2d
Juan Pablo Alba Fernandez <juanpialba@gmail.com>
2022-09-25 16:58:13 -0300

    primer commit

---
+++ js/primeraClase.js
@@ -0,0 +1,32 @@
+console.log("Hola Mundo");
+document.write("Hola mundo");
+
+// console.log("Este es un 2do mensaje");
+
+const PI = 3.14;
+console.log(typeof PI);
+
+let suma = 0;
+// numero + string = string
+let valorDeEntrada = Number(prompt("Indique un Valor"));
+console.log(valorDeEntrada);
+console.log(typeof valorDeEntrada);
+
+suma = suma + valorDeEntrada;
+
+valorDeEntrada = Number(prompt("Indique un Valor"));
+console.log(valorDeEntrada);
+
+suma = suma + valorDeEntrada;
+
+valorDeEntrada = Number(prompt("Indique un Valor"));
+console.log(valorDeEntrada);
+
+suma = suma + valorDeEntrada;
+
+valorDeEntrada = Number(prompt("Indique un Valor"));
+console.log(valorDeEntrada);
+
+suma = suma + valorDeEntrada;
+
+console.log("La Suma es de " + suma);
